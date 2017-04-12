189f1cfa0f8fbda28a4c5933ed03af9718e66476
Ajaya Pradhan <ajaya.pradhan@hcl.com>
2017-04-12 10:09:07 +0530

    changing second time

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ import java.io.*;
 public class HelloWorld{
   public static void main(String args[]){
     System.out.println("HelloWorld Sample Program in helloworldjava repository - GitHub");
-	System.out.println("New Line added for changes: 9:33 AM")
+	System.out.println("New Line added for changes: 9:33 AM");
+	System.out.println("Only push command used for changes: 9:53 AM");
    }
