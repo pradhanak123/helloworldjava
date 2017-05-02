bd92d2e6820c717537998deb18694cd1b11b1d8e
Ajaya Pradhan <ajaya.pradhan@hcl.com>
2017-05-02 16:46:23 +0530

    saving the changes to github-method2

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,10 +7,16 @@ public class HelloWorld{
 	System.out.println("Only push command used for changes: 9:53 AM");
 	HelloWorld obj = new HelloWorld();
 	obj.method1();
+	obj.method2();
    }
    // simple method definition
+   
    void method1(){
 	   System.out.println("INside the method1");
+	   
+   }
+   void method2(){
+	   System.out.println("INside the method2");
 	   
    }
 }   
