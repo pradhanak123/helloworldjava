995124592b908cc6b9fe01a1cdd6e37fad1410c8
Ajaya Pradhan <ajaya.pradhan@hcl.com>
2017-05-02 13:43:49 +0530

    saving the changes to github

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,4 +5,13 @@ public class HelloWorld{
     System.out.println("HelloWorld Sample Program in helloworldjava repository - GitHub");
 	System.out.println("New Line added for changes: 9:33 AM");
 	System.out.println("Only push command used for changes: 9:53 AM");
+	HelloWorld obj = new HelloWorld();
+	obj.method1();
    }
+   
+   void method1(){
+	   System.out.println("INside the method1");
+	   
+   }
+}   
+   
